2ecb0c64d6cdcde6de6fde7c0ca0edb166231f0e
Thiago Temple <vintem@gmail.com>
2013-08-19 12:40:22 -0400

    added script chocolatey

---
+++ choc_devsetup.ps1
@@ -0,0 +1,24 @@
+# Chocolatey
+iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+
+# install applications
+cinst git
+cinst notepadplusplus 
+cinst sublimetext2 
+cinst SublimeText2.PackageControl
+cinst ruby
+cinst ruby.devkit
+cinst python 
+cinst nodejs
+cinst putty
+cinst Firefox
+cinst fiddler4
+cinst filezilla
+cinst PhantomJS
+cinst paint.net
+cinst Compass
+cinst poshgit 
+cinst dropbox
+cinst winmerge
+cinst winrar -Version 4.20.0
+cinst mongodb
